aca45e82aa556e88a6e22b9ce5a3b89cb3fa0db8
Priyanka Ganesan <gpriyankapms@gmail.com>
2024-06-21 22:48:51 -0500

    hero typing

--- birthday-website/src/components/Hero.js
+++ birthday-website/src/components/Hero.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import heroImage from '../assets/hero-image.jpg'; // Ensure this path is correct
 import './Hero.css'; // Ensure you have this CSS file in the same directory
 
-const Hero = () => (
-  <div className="hero-section">
-    <img src={heroImage} alt="Hero" className="hero-image" />
-  </div>
-);
+const Hero = () => {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    const poem = "Close your eyes and \n make your wishes, \n\n I will make them come true \n in a thousand kisses!";
+
+    //,\n\nI will make them come true \nin a thousand kisses!
+
+    let currentIndex = 0;
+    const poemTimer = setInterval(() => {
+      if (currentIndex <= poem.length) {
+        setText(poem.substring(0, currentIndex));
+        currentIndex++;
+      } else {
+        clearInterval(poemTimer);
+        setTimeout(() => {
+          animateBirthdayMessage();
+        }, 1000); // Delay before starting the birthday message animation
+      }
+    }, 100); // Adjust the interval for typing speed
+
+    const animateBirthdayMessage = () => {
+      const birthdayMessage = "HAPPY 27TH BIRTHDAY PRASANNA!";
+      let index = 0;
+      const birthdayTimer = setInterval(() => {
+        if (index <= birthdayMessage.length) {
+          setText(poem + "\n\n" + birthdayMessage.substring(0, index));
+          index++;
+        } else {
+          clearInterval(birthdayTimer);
+        }
+      }, 100); // Adjust the interval for typing speed
+    };
+
+    return () => {
+      clearInterval(poemTimer);
+    };
+  }, []);
+
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <img src={heroImage} alt="Hero" className="hero-image" />
+        <div className="animated-text">
+          <p>{text}<span className="cursor"></span></p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
